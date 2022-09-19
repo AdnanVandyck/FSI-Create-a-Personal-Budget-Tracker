8d041238f79295c5a0bf2d5d5288f0a8aec88cdc
Adnan <adnanvandyck@adnans-mbp.mynetworksettings.com>
2022-09-19 19:17:37 -0400

    Convert user input to number

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ let annualExpenses = 0
 
 // Your Code Here
 window.prompt(weeklyExpenseQuestions[0])
+
+// User Answers
+let answer = window.prompt(weeklyExpenseQuestions[0])
+console.log(answer)
+
+
+//Turn User Input from Sting to Number
+let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+let numberAnswer = parseFloat(stringAnswer)
+console.log(numberAnswer)
+
